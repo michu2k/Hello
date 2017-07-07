7672bb82d11ec6295870dbf4b31fbd3785e14a31
michu2k <michalo2k@op.pl>
2017-07-07 20:55:39 +0200

    Update v1.1

--- js/theme.js
+++ js/theme.js
@@ -1,27 +1,28 @@
 // Hamburger menu
 $(function(){
-    $("#mobile-btn").click(function() { 
-        var nav = $("#main-navigation");
+    $('#mobile-btn').click(function() { 
+        let nav = $('#main-navigation');
         
-        if (nav.css("right") == 0 + "px") 
-            nav.css("right", "-250px");
-        else nav.css("right", "0");
+        if (nav.css('right') == 0 + 'px') 
+            nav.css('right', '-250px');
+        else nav.css('right', '0');
 
-        $(this).toggleClass("toggled");
+        $(this).toggleClass('toggled');
+        $('#mobile-background').toggleClass('toggled');
     });   
 });
 
 // Dropdown
 $(function(){
-    var dropdownclass = ".dropdown-menu";
-    var toggleclass = "toggled";
+    let dropdownclass = '.dropdown-menu';
+    let toggleclass = 'toggled';
 
     $(window).click(function(e) {
-        if ( !$("li.dropdown > a").is(e.target) && $(window).width() > 992 )
+        if ( !$('li.dropdown > a').is(e.target) && $(window).width() > 992 )
             $(dropdownclass).removeClass(toggleclass);
     });
 
-    $("li.dropdown > a").click(function(e) {
+    $('li.dropdown > a').click(function(e) {
         e.preventDefault();  
 
         $(dropdownclass)
